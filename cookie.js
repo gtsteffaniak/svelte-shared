0adf55cc4633c2a17ba0665bb9e774f335ac33be
Graham Steffaniak <graham.steffaniak@autodesk.com>
2023-09-26 15:26:52 -0500

    updated cookie find

--- cookie.js
+++ cookie.js
@@ -1,7 +1,5 @@
 export function getCookie(name) {
-  let cookie = document.cookie
-    .split(";")
-    .find((cookie) => cookie.includes(name + "="));
+  let cookie = document.cookie.split(";").find((cookie) => cookie.includes(name + "="));
   if (cookie != null) {
     return cookie.split("=")[1];
   }
