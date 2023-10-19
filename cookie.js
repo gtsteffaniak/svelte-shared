d04ccaf20da814e533c4b0456b57573bec826f33
Graham Steffaniak <42989099+gtsteffaniak@users.noreply.github.com>
2023-10-19 10:43:49 -0500

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,9 +1,18 @@
+
+// returns cookie string if available, otherwise empty string
 export function getCookie(name) {
-  let cookie = document.cookie.split(";").find((cookie) => cookie.includes(name + "="));
+  let cookie = document.cookie
+    .split(";")
+    .find((cookie) => cookie.includes(name + "="));
   if (cookie != null) {
     return cookie.split("=")[1];
   }
   return ""
+}
+
+// delete cookie by name
+export function deleteCookie(name) {
+  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;domain=.gportal.link`;
 }
 
 // Set the cookie with the API key value
