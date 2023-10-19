342105ec95e2616e952ffc19ae2335ae76bd825b
Graham Steffaniak <graham.steffaniak@autodesk.com>
2023-10-19 12:38:17 -0500

    updated functions

--- cookie.js
+++ cookie.js
@@ -12,7 +12,7 @@ export function getCookie(name) {
 
 // delete cookie by name
 export function deleteCookie(name) {
-  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;domain=`+window.location.hostname;
+  document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;domain=.`+rootDomain();
 }
 
 // Set the cookie with the API key value
@@ -21,5 +21,16 @@ export function setCookie(name, value) {
 
   const expirationDate = new Date();
   expirationDate.setFullYear(expirationDate.getFullYear() + 1);
-  document.cookie = `${name}=${value};expires=${expirationDate.toUTCString()};path=/;domain=`+window.location.hostname;
+  document.cookie = `${name}=${value};expires=${expirationDate.toUTCString()};path=/;domain=.`+rootDomain();
+}
+
+function rootDomain() {
+  let hostname = window.location.hostname
+  let parts = hostname.split(".");
+  if (parts.length <= 2)
+    return hostname;
+  parts = parts.slice(-3);
+  if (['co','com'].indexOf(parts[1]) > -1)
+    return parts.join('.');
+  return parts.slice(-2).join('.');
 }
